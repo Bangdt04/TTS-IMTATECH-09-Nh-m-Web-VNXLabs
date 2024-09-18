f8ec63f95a56180b53c514341a702054058f9aaf
vantuyen89 <vantuyenfpl@gmail.com>
2024-09-18 10:56:04 +0700

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css'
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LayoutCLient from "./layout/LayoutCLient";
+import ProductDetailPage from "./pages/client/product-detail/ProductDetailPage";
 function App() {
-  return ( 
-      <>
-          <h1>Chúc các bạn làm bài tốt</h1> 
-      </>
-  )
-      
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LayoutCLient />}>
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
